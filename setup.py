768aff6496c03e9b9fb61f34ffed3b465684f0a9
Songyuan Zhang <syzhang092218@gmail.com>
2022-12-16 16:06:14 -0500

    add set up and requirements

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name="robot_golf",
+    version="0.0.0",
+    description='Playing robot with a KUKA robot arm.',
+    author="Songyuan Zhang, Mingxin Yu",
+    author_email="szhang21@mit.edu, yumx35@mit.edu",
+    url="https://github.com/syzhang092218-source/robot-golf/",
+    install_requires=[],
+    packages=find_packages(),
+)
